7e9eadebabeb87bb1a3c69da4acf6d8e03289096
AzureAD\JoseManuelRodriguezC <swagerpiv@gmail.com>
2022-10-25 12:15:26 +0200

    color azul+

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import{Text,Image, TextInput,View, StyleSheet, onChangeText,Button, Alert} from 'react-native';
 
 const styles = StyleSheet.create({
+
   Verde: {
     color: 'green',
     fontWeight: 'bold',
@@ -28,16 +29,16 @@ export function mostrarResult(edad){
 
 }
 
-
    
 
-const nombre = 'Jose'
-const apellidos= 'cortes'
+const nombre = <Text style ={[styles.Azul]}>Jose Manuel</Text>;
+const apellidos = <Text style ={[styles.Azul]}>Rodriguez Cortes</Text>;
 
 
 export default function App(){
     const color=null
     const [edad, setEdad] = useState(' ')
+    
 
     
    
@@ -46,7 +47,7 @@ export default function App(){
    <View>
      
      <Text  > </Text>
-      <Text  style={styles.Azul} >{"hola "+ nombre +" "+ apellidos} </Text>
+      <Text>hola, me llamo {nombre} {apellidos} </Text>
 
       <Text  > </Text>
 
@@ -68,9 +69,10 @@ export default function App(){
       
       <Text  > </Text>
       <Button
-        title="FINALIZAR"onPress={() =>  {mostrarResult(edad)}}/>
+        title="FINALIZAR"
+        onPress={() => {mostrarResult(edad)}}/>
       
-      <Text style={{ color: 'black'}}  >{mostrarResult(edad)}</Text>
+      <Text>{mostrarResult(edad)}</Text>
 
    </View>
     
