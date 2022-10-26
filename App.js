9f8e45400cbc243be1fde23f219949a1d7a57338
JoseManuelRodriguezCortes <92427972+yonlee03@users.noreply.github.com>
2022-10-26 14:51:05 +0200

    parcheo fallos

--- App.js
+++ App.js
@@ -1,9 +1,46 @@
-
 import React, { useState } from 'react';
 import { Text, Image, TextInput, View, StyleSheet, onChangeText, Button, Alert } from 'react-native';
 
-const styles = StyleSheet.create({
 
+
+
+
+export default function App() {
+  const nombre = <Text style={[styles.Azul]}>Jose Manuel</Text>;
+  const apellidos = <Text style={[styles.Azul]}>Rodriguez Cortes</Text>;
+  const [edad, setEdad] = useState(' ')
+  const [texto, setTexto] = useState(null);
+  const resultado = () => {
+    (edad < 18 ? setTexto(<Text style={styles.Rojo}>Menor de edad</Text>) : setTexto(<Text style={styles.Verde}>Mayor de edad</Text>))
+  }
+
+  return (
+    <View>
+      <Text style={styles.Texto1}>Mi nombre es {nombre} y me apellido {apellidos} </Text>
+      
+      <Text style={styles.Centrado}>DIME TU EDAD</Text>
+      <Text  > </Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "#3198EE",
+          borderWidth: 2,
+        }}
+        value={edad}
+        onChangeText={edad => setEdad(edad)}
+        defaultValue="EDAD" />
+      <Text  > </Text>
+      <Button
+        title="FINALIZAR"
+        onPress={resultado} />
+      <Text style={styles.Centrado}>{texto}</Text>
+    </View>
+
+  );
+
+}
+
+const styles = StyleSheet.create({
   Verde: {
     color: "green",
     fontWeight: 'bold',
@@ -16,80 +53,18 @@ const styles = StyleSheet.create({
   },
   Azul: {
     color: "#3393FF",
-    fontWeight: 'bold',
-    
+    fontWeight: 'bold',   
   },
-
   Centrado: {
     color: "#58E0F6",
     fontWeight: 'bold',
     fontSize: 15,
     textAlign: "center",
+    marginTop:20,
   },
-
   Texto1:{
     fontSize: 13,
     textAlign: "center",
+    marginTop:20,
   }
-
 });
-
-
-
-
-const nombre = <Text style={[styles.Azul]}>Jose Manuel</Text>;
-const apellidos = <Text style={[styles.Azul]}>Rodriguez Cortes</Text>;
-
-
-export default function App() {
-  
-  const [edad, setEdad] = useState(' ')
-  const [Texto, setTexto] = useState(null);
-
-  const resultado = () => {
-    (edad < 18 ? setTexto(<Text style={styles.Rojo}>Menor de edad </Text>) : setTexto(<Text style={styles.Verde}>Mayor de edad </Text>))
-  }
-
-
-
-  return (
-
-    <View>
-
-      <Text  > </Text>
-      <Text style={styles.Texto1}>Mi nombre es {nombre} y me apellido {apellidos} </Text>
-
-      <Text  > </Text>
-
-      <Text style={styles.Centrado}>DIME TU EDAD</Text>
-
-
-      <Text  > </Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: "#3198EE",
-          borderWidth: 2,
-
-
-        }}
-        value={edad}
-        onChangeText={edad => setEdad(edad)}
-        defaultValue="EDAD" />
-
-      <Text  > </Text>
-      <Button
-        title="FINALIZAR"
-        onPress={resultado} />
-
-      <Text  > </Text>
-
-      <Text style={styles.Centrado}>{Texto}</Text>
-
-    </View>
-
-  );
-
-}
-
-
