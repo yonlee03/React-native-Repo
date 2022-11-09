1f217b83306887d305fbb061b89ed2392b895cbf
JoseManuelRodriguezCortes <92427972+yonlee03@users.noreply.github.com>
2022-11-09 16:07:41 +0100

    Form1

--- App.js
+++ App.js
@@ -1,43 +1,68 @@
 import React, { useState } from 'react';
-import { Text, Image, TextInput, View, StyleSheet, onChangeText, Button, Alert } from 'react-native';
-
-
-
-
+import { Text, Image, TextInput, View, StyleSheet, Button, Switch } from 'react-native';
 
 export default function App() {
-  const nombre = <Text style={[styles.Azul]}>Jose Manuel</Text>;
-  const apellidos = <Text style={[styles.Azul]}>Rodriguez Cortes</Text>;
+  const [nombre, setNombre] = useState(' ')
+  const [apellidos, setApellidos] = useState(' ')
   const [edad, setEdad] = useState(' ')
+  const [correo, setCorreo] = useState(' ')
   const [texto, setTexto] = useState(null);
+  const [isEnabled, setIsEnabled] = useState();
   const resultado = () => {
-    (edad < 18 ? setTexto(<Text style={styles.Rojo}>Menor de edad</Text>) : setTexto(<Text style={styles.Verde}>Mayor de edad</Text>))
+    (edad < 18 ? setTexto(<Text style={styles.Rojo}>Menor de edad </Text>) : setTexto(<Text style={styles.Verde}>Mayor de edad </Text>))
   }
 
   return (
-    <View>
-      <Text style={styles.Texto1}>Mi nombre es {nombre} y me apellido {apellidos} </Text>
+    <View style={styles.CentradoV}>
+      <Text style={styles.Centrado}>FORM</Text>
+
+      <View style={styles.TextoV}>
+        <Text style={styles.Texto}>DIME TU NOMBRE  </Text>
+        <TextInput style={styles.TextImp}
+          value={nombre}
+          onChangeText={nombre => setNombre(nombre)}
+          defaultValue="Nombre" />
+      </View>
+      <View style={styles.TextoV}>
+        <Text style={styles.Texto}>DIME TU APELLIDO</Text>
+        <TextInput style={styles.TextImp}
+          value={apellidos}
+          onChangeText={apellidos => setApellidos(apellidos)}
+          defaultValue="APELLIDO" />
+      </View>
+
+      <View style={styles.TextoV}>
+        <Text style={styles.Texto}>DIME TU EDAD        </Text>
+        <TextInput style={styles.TextImp}
+          value={edad}
+          onChangeText={edad => setEdad(edad)}
+          defaultValue="EDAD" />
+      </View>
+
+      <View style={styles.TextoV}>
+        <Text style={styles.Texto}>DIME TU CORREO  </Text>
+        <TextInput style={styles.TextImp}
+          value={correo}
+          onChangeText={correo => setCorreo(correo)}
+          defaultValue="CORREO" />
+      </View>
+
+      <View style={styles.TextoV}>
+      <Text style={styles.Texto}>MEN                         </Text>
+        <Switch
+          Text={{ false: 'white', true: 'grey' }}
+          trackColor={{ false: 'white', true: 'grey' }}
+          thumbColor={isEnabled ? 'darkblue' : 'yellow'}
+          onValueChange={() => setIsEnabled(previusState => !previusState)}
+          value={isEnabled} />
+          <Text style={styles.Texto}>                  NO MEN</Text>
+      </View>
+      <Button 
+          title="FINALIZAR"
+          onPress={resultado} />
       
-      <Text style={styles.Centrado}>DIME TU EDAD</Text>
-      <Text  > </Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: "#3198EE",
-          borderWidth: 2,
-        }}
-        value={edad}
-        onChangeText={edad => setEdad(edad)}
-        defaultValue="EDAD" />
-      <Text  > </Text>
-      <Button
-        title="FINALIZAR"
-        onPress={resultado} />
-      <Text style={styles.Centrado}>{texto}</Text>
     </View>
-
   );
-
 }
 
 const styles = StyleSheet.create({
@@ -53,18 +78,49 @@ const styles = StyleSheet.create({
   },
   Azul: {
     color: "#3393FF",
-    fontWeight: 'bold',   
+    fontWeight: 'bold',
   },
   Centrado: {
+    marginTop:170,
+    color: "#58E0F6",
+    fontWeight: 'bold',
+    fontSize: 50,
+    alignItems: 'center',
+    textAlign: "center",
+  },
+  CentradoV: {
+    flex:1,
+    backgroundColor:'black',
     color: "#58E0F6",
     fontWeight: 'bold',
     fontSize: 15,
+    alignItems: 'center',
     textAlign: "center",
-    marginTop:20,
   },
-  Texto1:{
-    fontSize: 13,
+  TextoV: {
+    flexDirection: 'row',
+    marginTop: 20,
+    fontSize: 15,
+  },
+  Texto: {
+    marginTop: 9,
+    fontSize: 15,
+    
+    marginLeft:20,
+    marginBottom:10,
+    color:'white'
+  },
+  TextImp: {
+    marginLeft: 20,
+    height: 40,
+    width: 170,
+    color:'white',
     textAlign: "center",
-    marginTop:20,
-  }
+    borderColor: "#3198EE",
+    borderWidth: 2,
+  },
+  Boton: {
+    marginTop: 9,
+    fontSize: 15,
+  },
 });
